51356b295757fb128dcf2c3449a076ddb0f5be04
Michael Cole <michael@bigsmall.io>
2018-02-13 13:40:32 -0800

    Web Workers

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# demo_webworkers_serviceworkers
-Demo code
+# Web Workers, Service Workers, and PWA's
+
+This is demo code for a tech talk.
+
+https://slides.com/michaelcole/deck-1-6-5/live#/
+
+Run this code in your browser by serving it with a web server.  E.g.
+
+```
+git clone
+cd 
+npm install http-server -g
+http-server .
+```
+
+You can use any web server you like.  #[http-server](https://www.npmjs.com/package/http-server) is the easy button.
---
+++ webworker/worker.js
@@ -0,0 +1,37 @@
+// Listen for messages from main page w/ string for outgoing messages.
+var task = 'Buy pretty things';
+var r = self.addEventListener('message', function(e) {
+  console.log('... worker got message ' + e.data);
+  task = e.data;
+}, false);
+
+// Fibonacci numbers generator.  
+// F(0) = 0, F(1) = 1, F(x) = (F(x-2) + F(x-1).
+function F(x) { 
+  if (x<2) return x;  return F(x-1) + F(x-2); 
+}
+
+// Send messages with Fibonacci numbers and task string
+var i = 0;
+setInterval(function() {
+  self.postMessage(F(i++) + ' ' + task);
+}, 200);
+
+// Don't use while(true) {}, or message events won't be processed
+
+
+// SIDEBAR:  You can import scripts.  Note it's not require() or import...
+importScripts("//cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.5/lodash.min.js");
+console.log("SIDEBAR: importScripts", _);
+
+// SIDEBAR:  You can make network requests
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) { 
+      console.log("SIDEBAR: XMLHttpRequest for README.md");
+      console.log(xhttp.responseText.slice(0, xhttp.responseText.indexOf("\n")) );
+    }
+};
+xhttp.open("GET", "/README.md", true);
+xhttp.send();
+
